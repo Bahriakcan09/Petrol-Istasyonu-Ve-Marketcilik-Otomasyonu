1875c8c5bf3c53a91ba6ac9ec143a0b97ffe1fe6
Bahri AKÇAN <akcan750bahri09@gmail.com>
2023-04-17 19:43:39 +0300

    LoginForm Firebase ile bağlandı

--- LoginForm.cs
+++ LoginForm.cs
@@ -7,6 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Firebase.Auth;
+using Firebase.Auth.Providers;
+using Firebase.Auth.Repository;
 
 namespace Petrol_Istasyonu_Ve_Marketcilik_Otomasyonu
 {
@@ -16,10 +19,45 @@ namespace Petrol_Istasyonu_Ve_Marketcilik_Otomasyonu
         {
             InitializeComponent();
         }
-
-        private void LoginBtn_Click(object sender, EventArgs e)
+        private async void LoginBtn_Click(object sender, EventArgs e)
         {
+            bool login = await loginAsync();
+            if (login)
+            {
+                MessageBox.Show("Giris Basarili");
+            }
+            else
+            {
+                MessageBox.Show("Giris Basarisiz");
+            }
+        }
+        private async Task<bool> loginAsync()
+        {
+            object userCredential;
+            var config = new FirebaseAuthConfig
+            {
+                ApiKey = "AIzaSyBPP5gvm8JvBT43dAsqUYlOXxy_GUcMyAU",
+                AuthDomain = "petrol-ve-marketcilik.firebaseapp.com",
+                Providers = new FirebaseAuthProvider[]
+                {
+                    new GoogleProvider().AddScopes("email"),
+                    new EmailProvider()
+
+                },
+            };
+
+            var client = new FirebaseAuthClient(config);
+            try
+            {
+                 userCredential = await client.SignInWithEmailAndPasswordAsync(usernameTxt.Text, passwordTxt.Text);
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+            return userCredential != null;
 
         }
     }
+    
 }
