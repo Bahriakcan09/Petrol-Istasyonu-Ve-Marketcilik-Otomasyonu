46827dd55ab6f1c45d287381f45100f310537502
Bahri AKÇAN <akcan750bahri09@gmail.com>
2023-04-26 23:28:39 +0300

    İnternetCheckClass oluşan bi hata yüzünden tekrar düzenlendi.

---
+++ internetCheckClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Petrol_Istasyonu_Ve_Marketcilik_Otomasyonu
+{
+    internal class internetCheckClass
+    {
+    }
+}
